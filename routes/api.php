14160c97093ae826079b5c83fd60c6f2a993f3b0
Rodolfo Bais <rodolfo282@gmail.com>
2026-07-09 18:00:45 -0300

    Add rate limiting to write endpoints and an OpenAPI spec

--- routes/api.php
+++ routes/api.php
@@ -4,5 +4,9 @@ use App\Infrastructure\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/jobs', [JobController::class, 'index']);
-Route::post('/jobs', [JobController::class, 'store']);
-Route::post('/subscriptions', [JobController::class, 'subscribe']);
+
+// Stricter throttle than the default 60/min API-wide limit: these two write
+// endpoints can trigger an email send (job creation notifies subscribers,
+// subscribing registers a new recipient) with no auth in front of them.
+Route::post('/jobs', [JobController::class, 'store'])->middleware('throttle:10,1');
+Route::post('/subscriptions', [JobController::class, 'subscribe'])->middleware('throttle:10,1');
